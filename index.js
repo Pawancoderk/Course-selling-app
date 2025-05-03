60234a3aae380064c222b08ad3950c884a6214c4
Pawan <imrealpawan@gmail.com>
2025-05-03 17:39:14 +0530

    Step 5 - Added admin routes

--- index.js
+++ index.js
@@ -3,8 +3,10 @@ const app = express();
 
 const{userRouter} = require("./routes/user.js")
 const{courseRouter} = require("./routes/course.js")
+const {adminRouter} = require("./routes/admin.js")
 
 app.use("/api/v1/user",userRouter);
 app.use("/api/v1/course",courseRouter)
+app.use("/api/v1/admin",adminRouter);
 
 app.listen(3000)
